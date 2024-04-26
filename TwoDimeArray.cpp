48cc90dfe1c1a9b62caf1fd30f16c8c75b323da2
Omikhe <msakamike84@gmail.com>
2024-04-26 22:44:19 +0200

    collect 2D arr elements from user input

--- TwoDimeArray.cpp
+++ TwoDimeArray.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
 using namespace std;
-// double arraySize(double twoDimearr[2][2]);
 int main()
 {
-    // double arr[3][4];
-    double arr[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    // double arraySize(double twoDimearr[2][2]);
+    double arr[3][4];
+    // double arr[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
 
     // number of rows
     int rowNum = sizeof(arr) / sizeof(arr[0]);
-    cout << "rows: " << rowNum << endl;
+    // cout << "rows: " << rowNum << endl;
 
     // number of columns
     int colNum = sizeof(arr) / sizeof(arr[0][0]);
-    cout << "columns: " << colNum << endl;
+    // cout << "columns: " << colNum << endl;
 
-    cout << "elements per row: " << colNum / rowNum << endl;
-    cout << "  " << endl;
-    cout << "  " << endl;
+    // cout << "elements per row: " << colNum / rowNum << endl;
+    // cout << "  " << endl;
+    // cout << "  " << endl;
     int n = colNum / rowNum;
+
+    for (int i = 0; i < rowNum; i++)
+    {
+        cout << "row: " << i << endl;
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
 
     for (int i = 0; i < rowNum - 1; i++)
     {
