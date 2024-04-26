a078c2d230c82b1e18f144ddcf60d6c14aaf8386
Omikhe <msakamike84@gmail.com>
2024-04-26 22:00:00 +0200

    Mess with 2D array successfully

---
+++ TwoDimeArray.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+// double arraySize(double twoDimearr[2][2]);
+int main()
+{
+    // double arr[3][4];
+    double arr[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+
+    // number of rows
+    int rowNum = sizeof(arr) / sizeof(arr[0]);
+    cout << "rows: " << rowNum << endl;
+
+    // number of columns
+    int colNum = sizeof(arr) / sizeof(arr[0][0]);
+    cout << "columns: " << colNum << endl;
+
+    cout << "elements per row: " << colNum / rowNum << endl;
+    cout << "  " << endl;
+    cout << "  " << endl;
+    int n = colNum / rowNum;
+
+    for (int i = 0; i < rowNum - 1; i++)
+    {
+        for (int j = 0; j < ((colNum - rowNum) - 1); j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
