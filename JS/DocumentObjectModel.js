779b868567fe9eaf91861f2fabba857ebc5ffaf0
Omikhe <msakamike84@gmail.com>
2024-04-30 23:36:04 +0200

    add DOM notes

---
+++ JS/DocumentObjectModel.js
@@ -0,0 +1,12 @@
+/*
+# DOM = Document Object Model
+
+A JavaScript Object that represents the page you in the web browser
+and provides you with an API to interact with it.
+A web browser constructs the DOM when it loads an HTML document,
+and structures all the elements in a tree-like representation
+JavaScript can access the DOM to dynamically change the content,
+structure, and style of a web page.
+
+- Bro Code [https://www.youtube.com/watch?v=NO5kUNxGIu0]
+*/
