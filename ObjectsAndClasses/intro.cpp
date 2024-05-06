4f80d4abd81af82b333d6200e5281d5530a9536a
Omikhe <msakamike84@gmail.com>
2024-05-06 18:37:10 +0200

    create file and add some boilerplate

---
+++ ObjectsAndClasses/intro.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // your code here
+    return 0;
+}
+
+class Circle
+{
+public:
+    double radius;
+
+    Circle()
+    {
+        cout << "Constructor function here!" << endl;
+        radius = 1;
+    }
+
+    Circle(double newRadius)
+    {
+        }
+};
