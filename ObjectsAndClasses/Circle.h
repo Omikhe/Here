16fc36739a1303eb60d04210df7bc33bd32d135a
Omikhe <msakamike84@gmail.com>
2024-05-08 01:04:07 +0200

    implement header

---
+++ ObjectsAndClasses/Circle.h
@@ -0,0 +1,16 @@
+class Circle
+{
+public:
+    // radius data field
+    double radius;
+
+    // constructor a default circle object
+    Circle();
+
+    // constructor
+    Circle(double);
+
+    // public member function to get the area of the circle
+    double getArea();
+
+}; // must place semicolon here
